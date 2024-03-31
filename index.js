332c8f378d0b47a037ff7393bbf4599de2f505b5
Wildan Zulfikar <hey@wzulfikar.com>
2024-04-01 02:49:53 +0300

    done!

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ bun ./index.js
 */
 (async () => {
   for (const [provider, { pricing_page, models, embedding_models }] of Object.entries(llmPrices)) {
-    if (provider != 'mistral') continue;
-    console.log(`- Fetching model prices for "${provider}". Pricing page: ${pricing_page}`)
+    console.log(`- Fetching model prices for "${provider}": ${pricing_page}`)
     const pricingPage = await fetch(pricing_page).then(res => res.text())
     const content = htmlToText(pricingPage).trim()
     const modelsPrices = await getPrices(provider, { models, embedding_models, content })
